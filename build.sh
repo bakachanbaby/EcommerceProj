e11558cf0b4416b191681674bcfce73d23498ee8
Nguyễn Trần Kiên <63496788+bakachanbaby@users.noreply.github.com>
2025-02-05 16:18:23 +0700

    Refactor build script and remove empty __init__.py files

--- build.sh
+++ build.sh
@@ -1,12 +1,13 @@
-#!/usr/bin/env bash
+#!/bin/bash
 # Exit on error
 set -o errexit
 
 # Modify this line as needed for your package manager (pip, poetry, etc.)
+python -m pip install --upgrade pip
 pip install -r requirements.txt
 
 # Convert static asset files
-python manage.py collectstatic --no-input
+python manage.py collectstatic --noinput
 
 # Apply any outstanding database migrations
 python manage.py migrate
